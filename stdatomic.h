a186d756f1a4cf380841f2a0b8d449f151f7f4d5
Zenny Chen <zenny_chen@sina.com>
2019-05-11 16:56:35 +0800

    Update stdatomic.h

--- stdatomic.h
+++ stdatomic.h
@@ -5,7 +5,7 @@
 #define ATOMIC_VAR_INIT(value)      (value)
 #define atomic_init(object, value)  (void)(*(object) = (value))
 
-typedef volatile int    atomic_int;
+typedef int    atomic_int;
 
 #ifdef __GNUC__
 #define atomic_store(object, desired)   (void)( *(volatile typeof(*(object)) *)(object) = (desired) )
@@ -22,14 +22,14 @@ typedef struct atomic_flag { bool _Value; } atomic_flag;
 extern bool atomic_flag_test_and_set(volatile atomic_flag *flag);
 extern void atomic_flag_clear(volatile atomic_flag *flag);
 
-extern int atomic_fetch_add(atomic_int *object, int operand);
-extern int atomic_fetch_sub(atomic_int *object, int operand);
-extern int atomic_fetch_or(atomic_int *object, int operand);
-extern int atomic_fetch_xor(atomic_int *object, int operand);
-extern int atomic_fetch_and(atomic_int *object, int operand);
+extern int atomic_fetch_add(volatile atomic_int *object, int operand);
+extern int atomic_fetch_sub(volatile atomic_int *object, int operand);
+extern int atomic_fetch_or(volatile atomic_int *object, int operand);
+extern int atomic_fetch_xor(volatile atomic_int *object, int operand);
+extern int atomic_fetch_and(volatile atomic_int *object, int operand);
 
-extern int atomic_exchange(atomic_int *object, int desired);
+extern int atomic_exchange(volatile atomic_int *object, int desired);
 
-extern bool atomic_compare_exchange_strong(atomic_int *object, int *expected, int desired);
-extern bool atomic_compare_exchange_weak(atomic_int *object, int *expected, int desired);
+extern bool atomic_compare_exchange_strong(volatile atomic_int *object, int *expected, int desired);
+extern bool atomic_compare_exchange_weak(volatile atomic_int *object, int *expected, int desired);
 
