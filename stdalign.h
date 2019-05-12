b70ac96255c5643a65127c946c1a58a152f2e58c
Zenny Chen <zenny_chen@sina.com>
2019-05-12 16:43:54 +0800

    Add files via upload

---
+++ stdalign.h
@@ -0,0 +1,12 @@
+﻿#ifndef __STDALIGN_H
+#define __STDALIGN_H
+
+#ifdef _MSC_VER
+
+#define alignof             _Alignof
+#define alignas(nBytes)     __declspec(align(nBytes))
+
+#endif // _MSC_VER
+
+#endif
+
---
+++ threads.h
@@ -0,0 +1,11 @@
+﻿#ifndef __MSVC_THREADS_H
+#define __MSVC_THREADS_H
+
+#ifdef _MSC_VER
+
+#define thread_local    __declspec(thread)
+
+#endif
+
+#endif
+
